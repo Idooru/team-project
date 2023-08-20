2b9f45ae2fed4eb66253c31efb8fec6028569a28
Idooru <shere1765@gmail.com>
2023-08-20 21:06:58 +0900

    input에 focus이벤트 발생시 라벨에 줄긋기

--- register/js/script.js
+++ register/js/script.js
@@ -1,29 +1,27 @@
 const formEle = $("form").children().toArray();
 
 formEle.forEach((element) => {
-  const labelEle = $(element).children("label");
-  const inputEle = $(element).children("input");
+  // const labelEle = $(element).children("label");
+  const $inputEle = $(element).children("input");
 
-  inputEle.on("focus", function () {
-    const lineEle = labelEle.children(".line");
-    lineEle.css({
-      visiblity: "visible",
-      "border-top": "4px solid black",
-      width: "110px",
-      height: "0",
-      "margin-top": "5px",
-    });
+  $inputEle.on("focus", function () {
+    const $lineEle = $inputEle.closest(".text__form").find("label .line");
+
+    let visibility = $lineEle.css("visibility");
+
+    visibility = "visible";
+
+    $lineEle.css({ visibility });
   });
 
-  inputEle.on("blur", function () {
-    const lineEle = labelEle.children(".line");
-    lineEle.css({
-      visiblity: "hidden",
-      "border-top": "4px solid black",
-      width: "110px",
-      height: "0",
-      "margin-top": "5px",
-    });
+  $inputEle.on("blur", function () {
+    const $lineEle = $inputEle.closest(".text__form").find("label .line");
+
+    let visibility = $lineEle.css("visibility");
+
+    visibility = "hidden";
+
+    $lineEle.css({ visibility });
   });
 
   if (element.localName === "button") {
