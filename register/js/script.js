ada43ddf72b9f3c67ad106c03b8a2a90800223d1
Idooru <shere1765@gmail.com>
2023-08-20 21:36:12 +0900

    코드 간략화

--- register/js/script.js
+++ register/js/script.js
@@ -1,21 +1,17 @@
 const formEle = $("form").children().toArray();
 
+const settingStyle = (style, ele, eleStyle) => {
+  eleStyle.visibility = style;
+  ele.css({ ...eleStyle });
+};
+
 formEle.forEach((element) => {
   const $inputEle = $(element).children("input");
   const $lineEle = $inputEle.closest(".text__form").find("label .line");
   const $lineEleStyle = { visibility: $lineEle.css("visibility") };
 
-  $inputEle.on("focus", function () {
-    $lineEleStyle.visibility = "visible";
-
-    $lineEle.css({ ...$lineEleStyle });
-  });
-
-  $inputEle.on("blur", function () {
-    $lineEleStyle.visibility = "hidden";
-
-    $lineEle.css({ ...$lineEleStyle });
-  });
+  $inputEle.focus(() => settingStyle("visible", $lineEle, $lineEleStyle));
+  $inputEle.blur(() => settingStyle("hidden", $lineEle, $lineEleStyle));
 });
 
 $("#user__id").focus();
