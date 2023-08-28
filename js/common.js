baf954acf8560806b3a6614edb3b179b63b33056
Idooru <shere1765@gmail.com>
2023-08-28 16:25:28 +0900

    load 메서드에 콜백 함수 추가

--- js/common.js
+++ js/common.js
@@ -1,2 +1,22 @@
-$(".header").load("./header/header.html");
+$(".header").load("./header/header.html", function () {
+  $("#search").on("click", function () {
+    $("#my-form").toggleClass("invisible");
+  });
+
+  if (JSON.parse(sessionStorage.getItem("logindUser"))) {
+    const $myPageEle = $("#my-page");
+
+    $myPageEle.children("i").attr({ class: "fa-solid fa-user" });
+    $myPageEle.parent("a").attr({ href: "../profile.html" });
+  }
+
+  const shopCart = false;
+
+  if (shopCart) {
+    const $shopCartEle = $("#shopcart");
+
+    $shopCartEle.children("i").attr({ class: "fa-solid fa-cart-shopping" });
+    $shopCartEle.parent("a").attr({ href: "../shopCart.html" });
+  }
+});
 $(".footer").load("./footer/footer.html");
