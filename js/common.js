f5141f35ac8e90ad2eb1a688c42c2d09f4690504
Idooru <shere1765@gmail.com>
2023-08-29 10:06:16 +0900

    로그아웃 기능

--- js/common.js
+++ js/common.js
@@ -5,9 +5,17 @@ $(".header").load("./header/header.html", function () {
 
   if (JSON.parse(sessionStorage.getItem("logindUser"))) {
     const $myPageEle = $("#my-page");
+    const $logoutEle = $("#logout");
 
     $myPageEle.children("i").attr({ class: "fa-solid fa-user" });
     $myPageEle.parent("a").attr({ href: "../profile.html" });
+
+    $logoutEle.attr({ style: "display: block; margin-left: 20px" });
+    $logoutEle.on("click", function () {
+      sessionStorage.clear();
+      alert("로그아웃 되었습니다.");
+      location.href = "../index.html";
+    });
   }
 
   const shopCart = false;
@@ -19,4 +27,5 @@ $(".header").load("./header/header.html", function () {
     $shopCartEle.parent("a").attr({ href: "../shopCart.html" });
   }
 });
+
 $(".footer").load("./footer/footer.html");
